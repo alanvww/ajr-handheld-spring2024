3520a869f2151b2f4cdbb38ac62fff91ca8c87c9
Alan Ren <10086000+alanvww@users.noreply.github.com>
2024-02-05 15:33:42 -0500

    Update hw#1

---
+++ week-2-hw/README.md
@@ -0,0 +1,3 @@
+# Handheld Homework #1: Slider Toy
+
+[Link](https://handheld-slidertoy-ajr.replit.app/)
---
+++ week-2-hw/main.js
@@ -0,0 +1,85 @@
+// Alan Ren
+// Handheld Homework #1: Slider Toy
+
+import './style.css';
+let video;
+let slider;
+
+// Get blur value from CSS variable
+const blurValue = getComputedStyle(document.documentElement)['--blur-value'];
+
+// Call this function when the user interacts with the page, like a button click
+document.getElementById('enableGyroscope').addEventListener('click', () => {
+	setupGyroscope();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+	video = document.getElementById('background-video');
+
+	video.volume = 0;
+
+	video.muted = false;
+	video.play();
+});
+
+document.getElementById('slider').addEventListener('input', (event) => {
+	let volume = event.target.value / 100;
+	video.volume = volume;
+	video.muted = false;
+	video.play();
+
+	// Update blur value in CSS variable
+	document.documentElement.style.setProperty(
+		'--blur-value',
+		(1 - volume) * parseFloat(blurValue)
+	);
+});
+document.getElementById('slider').addEventListener('change', () => {
+	let volumeInfo = document.getElementById('volume');
+	volumeInfo.innerHTML =
+		'Volume: ' + document.getElementById('slider').value + '%';
+});
+
+function handleOrientation(event) {
+	slider = document.getElementById('slider');
+
+	// Normalize beta to be within 0 to 90
+	// Clamp values outside this range
+	let beta = event.beta;
+	if (beta < 0) beta = 0;
+	if (beta > 90) beta = 90;
+
+	// Map beta (0 to 90) to a volume value (0.0 to 1.0)
+	let volume = beta / 90;
+	video.volume = volume;
+
+	// Adjust video play state
+	video.muted = false;
+	video.play();
+
+	// Calculate blur based on volume
+	let blurValue = (1 - volume) * 10;
+	video.style.filter = `blur(${blurValue}px)`;
+
+	// Update slider value
+	slider.value = volume * 100;
+}
+
+function setupGyroscope() {
+	if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+		// iOS devices
+		DeviceOrientationEvent.requestPermission()
+			.then((permissionState) => {
+				if (permissionState === 'granted') {
+					window.addEventListener('deviceorientation', handleOrientation);
+				} else {
+					console.log('Permission to access motion sensors was denied.');
+					// Implement fallback logic here
+				}
+			})
+			.catch(console.error);
+	} else {
+		// Non-iOS devices
+		window.addEventListener('deviceorientation', handleOrientation);
+	}
+}
